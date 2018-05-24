54cf3f2f3e073ba546baf434e1592c18a6d6788e
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2018-05-23 21:53:02 -0400

    Cleaned up the file.

--- termbin.sh
+++ termbin.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # Send output and errors to Termbin.com to print it out.
 
-# Check if the user want to create seperate standard output and error files.
-# Otherwise, put them in the same file.
-if [[ "$1" = "-s" ]] || [[ "$1" = "--seperate" ]]; then
+# Check if the user wants to just have stdout and stderr interacted with each other, or wants more processing done on the file.
+if [[ "$1" = "-s" ]] || [[ "$1" = "--seperate" ]] || [[ "$1" = "-c" ]] | [[ "$1" = "--concatinate" ]];then
   shift
 
   name=$1
@@ -11,17 +10,13 @@ if [[ "$1" = "-s" ]] || [[ "$1" = "--seperate" ]]; then
 
   $whole_command 1> "/tmp/.$name-stdout" 2> "/tmp/.$name-stderr"
 
-  cat "/tmp/.$name-stdout" | nc termbin.com 9999
-  cat "/tmp/.$name-stderr" | nc termbin.com 9999
-# Send standard output and standard error into one file sequentially.
-elif [[ "$1" = "-c" ]] | [[ "$1" = "--concatinate" ]]; then
-  shift
-
-  name=$1
-  whole_command=$@
-
-  $whole_command 1> "/tmp/.$name-stdout" 2> "/tmp/.$name-stderr"
-  echo -e "Input: $whole_command\nStandard Output:\n\n $(cat "/tmp/.$name-stdout")\n\nStandard Error:\n\n $(cat "/tmp/.$name-stderr")" | nc termbin.com 9999
+  # Check if the user either wants stdout and stderr to be seperate file on in a single file.
+  if [[ "$1" = "-s" ]] || [[ "$1" = "--seperate" ]]; then
+    cat "/tmp/.$name-stdout" | nc termbin.com 9999
+    cat "/tmp/.$name-stderr" | nc termbin.com 9999
+  elif [[ "$1" = "-c" ]] | [[ "$1" = "--concatinate" ]]; then
+   echo -e "Input: $whole_command\nStandard Output:\n\n $(cat "/tmp/.$name-stdout")\n\nStandard Error:\n\n $(cat "/tmp/.$name-stderr")" | nc termbin.com 9999
+  fi
 else
-  $@ 2>&1 | nc termbin.com 9999 
+  $@ 2>&1 | nc termbin.com 9999
 fi
