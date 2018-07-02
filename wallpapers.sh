ef62a35937e3543e8cf7031421ab3b2b3fbcb11c
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2018-07-02 14:48:12 -0400

    Added new scripts to goto store, mkscripts.

--- wallpapers.sh
+++ wallpapers.sh
@@ -3,8 +3,8 @@
 # Randomizes my wallpapers. 
 
 while true
-do 
-  nitrogen --head=1 --random --set-centered ~/Pictures/wallpapers_1280_1024/ 
-  nitrogen --head=0 --random --set-centered ~/Pictures/wallpapers_1680_1050/
+do
+  nitrogen --head=1 --random --set-centered ~/Pictures/wallpapers_1280_1024/ 2> /dev/null
+  nitrogen --head=0 --random --set-centered ~/Pictures/wallpapers_1680_1050/ 2> /dev/null
 sleep 600
 done
