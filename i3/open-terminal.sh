1ba3b67b70d150a0bd7de04476229c9b94c27400
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2018-09-27 04:34:01 -0400

    Added workspace variables for switching termintals.

--- i3/open-terminal.sh
+++ i3/open-terminal.sh
@@ -3,9 +3,10 @@
 # Created on Wednesday September 05, 2018.
 
 prog="$1"
+workspace="$2"
 
 if ( ! i3-msg -t get_tree | jq '.' | grep -E "\"class\": \"$prog-Termite\"" ); then
-  i3-msg "exec termite --exec=$prog --class=$prog-Termite"
+  i3-msg "workspace ${2}; exec termite --exec=$prog --class=$prog-Termite"
 else
   i3-msg "[class=\"$prog-Termite\"] focus"
 fi
