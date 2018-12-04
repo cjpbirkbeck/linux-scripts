c822f9fcda6ab946716756bfb71e375e88c20288
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2018-12-04 00:11:59 -0500

    Changed open-gui.sh and open-terminal.sh, added get-title.sh

--- i3/open-gui.sh
+++ i3/open-gui.sh
@@ -2,10 +2,22 @@
 # Opens programs in i3 if not already present. If they are already present, then they will set a mark.
 # Created on Wednesday September 05, 2018.
 
-prog="$1"
+de="$1"
+workspace="$2"
+prog="$3"
+
+echo "$de"
+echo "$workspace"
+echo "$prog"
+
+shift 3
+
+args="$*"
+
+echo "$args"
 
 if ( ! i3-msg -t get_tree | jq '.' | grep -E "\"class\": \"$prog\"" ); then
-  i3-msg "exec $prog"
+  i3-msg "workspace ${workspace}; exec ${de} ${prog} ${args}"
 else
   i3-msg "[class=\"$prog\"] focus"
 fi
--- i3/open-terminal.sh
+++ i3/open-terminal.sh
@@ -2,11 +2,14 @@
 # Opens programs in i3 if not already present. If they are already present, then they will set a mark.
 # Created on Wednesday September 05, 2018.
 
-prog="$1"
-workspace="$2"
+workspace="$1"
+prog="$2"
+
+shift 2
+args="$*"
 
 if ( ! i3-msg -t get_tree | jq '.' | grep -E "\"class\": \"${prog}\"" ); then
-  i3-msg "workspace ${workspace}; exec termite --exec=${prog} --class=${prog}"
+  i3-msg "workspace ${workspace}; exec termite --exec=\"${prog} ${args}\" --class=${prog}"
 else
   i3-msg "[class=\"${prog}\"] focus"
 fi
