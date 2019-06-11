fbec2c45d8a366e9dbfc76be1dd9894d56372670
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2019-06-11 15:53:09 -0400

    Removed general fzf files
    It just wouldn't work, at least not without many changes.

--- new/fzf/fzf-search.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This is a general script that allows searching certian files within some directories.
-# Then it uses fzf along with a preview from ranger's scope.sh to preview stuff if it can.
-# Note that this should read from a file for its search information.
-
-# "$OPENER" "$(find ${location} ${condition} | fzf --preview ${previewer})"
-
-
--- new/fzf/text-preview.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Converts primarily text-based documents into pure text the console can read.
-# Using a variety of tools, based on ranger's scope.sh script.
-
-file_path="${1}"
-pv_width
