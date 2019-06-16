d7599eb89ecc5479ab97543b4603c5fde509cff1
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2019-06-15 20:39:12 -0400

    Moved the config to its own dir.

--- new/tmux/utils.sh
+++ new/tmux/utils.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # Creates a new "util" tmux session, if none exists
 
-tmux has-session -t utils
-
-if [[ $? != 0 ]]; then
-    tmux -f utils.conf attach
+if tmux has-session -t Utils; then
+    echo "Utils session is active."
+    # tmux attach -t Utils
+else
+    tmux -f ./utils/.tmux.conf
 fi
