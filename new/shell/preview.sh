6e8c5521fd78891931b5471fbf89455cc4cabf27
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2019-06-21 23:03:36 -0400

    Made various changes to the previewer script

--- new/shell/preview.sh
+++ new/shell/preview.sh
@@ -1,25 +1,51 @@
 #!/usr/bin/env bash
+# Generates a previews of a file, based on its file extension and mimetype.
+# Otherwise, it prints out what finds.
 
-set -C -f -u
+set -C -f
 IFS=$'\n'
 
 # Arguments
 FILE_PATH="${1}"
-
+WINDOW_WIDTH="${2:-100}"
 FILE_EXT="${FILE_PATH##*.}"
-FILE_EXT_LOWER=$(echo ${FILE_EXT} | tr '[:upper:]' '[:lower:]')
+FILE_EXT_LOWER=$(echo "${FILE_EXT}" | tr '[:upper:]' '[:lower:]')
+
+# Default colour style, used by highlight.
+COLOUR_STYLE="pablo"
+PRINT_COLOURS="ansi"
+
+detect_colour() {
+    if [ -n "$COLORTERM" ]; then
+        PRINT_COLOURS="truecolor"
+    elif [ "$( tput colors )" -ge 256 ]; then
+        PRINT_COLOURS="xterm256"
+    fi
+}
 
 handle_extension() {
     case "${FILE_EXT_LOWER}" in
+        a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
+        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+            atool --list -- "${FILE_PATH}"
+            exit 1
+            ;;
         pdf)
-            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}"
-            exiftool "${FILE_PATH}"
+            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | fold
+            exiftool "${FILE_PATH}" | fold
+            exit 1
+            ;;
+        torrent)
+            transmission-show -- "${FILE_PATH}" | fold
+            exit 1
             ;;
         odt|ods|odp|sxw)
-            odt2txt "${FILE_PATH}"
+            odt2txt "${FILE_PATH}" --width="${WINDOW_WIDTH}" | fold
+            exit 1
             ;;
-        htm|html|html)
-            w3m -dump "${FILE_PATH}"
+        htm|html|xhtml)
+            w3m -dump "${FILE_PATH}" | fold
+            exit 1
             ;;
     esac
 }
@@ -29,27 +55,21 @@ handle_mime() {
 
     case "${MIMETYPE}" in
         text/* | */xml)
-            if [ "$( stat --printf='%s' -- "${FILE_PATH}" )" -gt "${HIGHLIGHT_SIZE_MAX}" ]; then
-                exit 2
-            fi
-            if [ "$( tput colors )" -ge 256 ]; then
-                local pygmentize_format='terminal256'
-                local highlight_format='xterm256'
-            else
-                local pygmentize_format='terminal'
-                local highlight_format='ansi'
-            fi
-            highlight --replace-tabs="${HIGHLIGHT_TABWIDTH}" --out-format="${highlight_format}" \
-                --style="${HIGHLIGHT_STYLE}" --force -- "${FILE_PATH}"
-            exit 2;;
+            detect_colour
+            highlight --replace-tabs="4" --out-format="${PRINT_COLOURS}" \
+                --style="${COLOUR_STYLE}" --wrap-simple --force -- "${FILE_PATH}"
+            exit 2
+            ;;
         # Images
         image/*)
             img2txt --gamma=0.6 -- "${FILE_PATH}" && exit 1
             exiftool "${FILE_PATH}"
+            exit 1
             ;;
         video/*|audio/*|application/octet-stream)
             mediainfo "${FILE_PATH}"
-            mediainfo "${FILE_PATH}"
+            exifinfo "${FILE_PATH}"
+            exit 1
             ;;
     esac
 }
@@ -57,14 +77,15 @@ handle_mime() {
 handle_fallback() {
     local mimetype="${1}"
 
-    echo "Error. Previewer cannot generate preview based on its extension or mimetype."
-    echo "File Extension: ${file_ext}"
-    echo "Mimetype: ${mimetype}"
+    echo "Previewer cannot generate preview based on its extension or mimetype."
+    echo "File Extension: ${FILE_EXT}"
+    echo "Mime Type: ${mimetype}"
+    exit 1
 }
 
 handle_extension
-MIMETYPE="$( file --deference --brief --mime-type -- "${FILE_PATH}")"
+MIMETYPE="$( file --dereference --brief --mime-type -- "${FILE_PATH}")"
 handle_mime "${MIMETYPE}"
-handle_fallback
+handle_fallback "${MIMETYPE}"
 
 exit 1
